c6bed65967694876785533c7787bfbbbc5983f92
Lucas <lucas.silva.t23@gmail.com>
2023-02-14 02:30:16 +0900

    Entrega del envio de reporte atraves de email

---
+++ ws-rest/src/main/java/ar/com/avaco/ws/rest/security/controller/ReporteRestController.java
@@ -0,0 +1,32 @@
+package ar.com.avaco.ws.rest.security.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.avaco.ws.rest.dto.JSONResponse;
+import ar.com.avaco.ws.service.ReporteEPService;
+
+@RestController
+public class ReporteRestController {
+
+	private ReporteEPService reporteEPService;
+	
+	@RequestMapping(value = "/reporte", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<JSONResponse> enviarReporte() {
+		this.reporteEPService.enviarReporte();
+		JSONResponse response = new JSONResponse();
+		response.setStatus(JSONResponse.OK);
+		return new ResponseEntity<JSONResponse>(response, HttpStatus.OK);
+	}
+
+	@Resource(name = "reporteEPService")
+	public void setService(ReporteEPService service) {
+		this.reporteEPService = service;
+	}
+}
---
+++ ws-rest/src/main/java/ar/com/avaco/ws/service/ReporteEPService.java
@@ -0,0 +1,7 @@
+package ar.com.avaco.ws.service;
+
+public interface ReporteEPService {
+	
+	public void enviarReporte();
+
+}
